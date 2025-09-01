7bab1428321a22653a231ae3e65aad1819833a12
Asad Ahmed <asadahmedk09@gmail.com>
2025-09-01 20:38:09 +0500

    Create Task3.cpp

---
+++ DSA LAB 2/Task3.cpp
@@ -0,0 +1,40 @@
+#include"iostream"
+using namespace std;
+int main()
+{
+  int rows = 0 , cols = 0;
+  cout << "enter the number of students : " << endl;
+  cin >> rows;
+  int** arr = new int*[rows];
+  int* colSize = new int[rows];
+  for(int i = 0 ; i < rows ; i++)
+  {
+    cout << "Enter the number of courses for student : " << i+1 << endl;
+    cin >> colSize[i] ;
+    arr[i] = new int[colSize[i]];
+  }
+  for(int i = 0 ; i < rows ; i++)
+  {
+    cout << "Enter the marks for student : " << i+1 << endl;
+    for(int j = 0 ; j < colSize[i] ; j++)
+    {
+      cout << "Enter the marks for course : " << j+1 << " : ";
+      cin >> arr[i][j];
+    }
+    cout << endl;
+  }
+  double* avgMarks = new double[rows];
+  for(int i = 0 ; i < rows ; i++)
+  {
+    for(int j = 0 ; j < colSize[i] ; j++)
+    { 
+      avgMarks[i]+=arr[i][j];
+    }
+    avgMarks[i]/=colSize[i];
+    cout << endl;
+  }
+  for(int i = 0 ; i < rows ; i++)
+  {
+    cout << "The average marks for student : " << i+1 << " : " << avgMarks[i] << endl;
+  }
+}
